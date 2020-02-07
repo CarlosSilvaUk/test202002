a11964f16f45728bf8e9ee78f1b07ef4c61d2185
Carlos Silva <carlos.farinha@outlook.com>
2020-02-07 01:24:37 +0000

    Test cases

--- Test/AWSLambda1.Tests/FunctionTest.cs
+++ Test/AWSLambda1.Tests/FunctionTest.cs
@@ -13,16 +13,21 @@ namespace AWSLambda1.Tests
 {
     public class FunctionTest
     {
-        [Fact]
-        public void TestToUpperFunction()
+        [Theory]
+        [InlineData(-10, 0)]
+        [InlineData(1001, 15.02)]
+        public void TestCases(decimal amount, decimal interestPaid)
         {
 
             // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
-
-            Assert.Equal("HELLO WORLD", upperCase);
+            var response = function.FunctionHandler(amount.ToString(), context);
+            if (!decimal.TryParse(response, out var result))
+            {
+                throw new Exception("Invalid response!");
+            }
+            Assert.Equal(interestPaid, result);
         }
     }
 }
