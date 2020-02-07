040e7ad586c736ccceff8b78a2fbac47e8ffa7b2
Carlos Silva <carlos.farinha@outlook.com>
2020-02-07 01:07:45 +0000

    Calculate interest rates

--- Test/app/Output/IOutput.cs
+++ Test/app/Output/IOutput.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text;
-
-namespace app.Output
+﻿namespace app.Output
 {
     public interface IOutput
     {
---
+++ Test/tests/Accounts/AccountTests.cs
@@ -0,0 +1,27 @@
+﻿using app.Accounts;
+using Xunit;
+
+namespace tests.Accounts
+{
+    public class AccountTests
+    {
+
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(1, 0.01)]
+        [InlineData(999, 0.01)]
+        [InlineData(1000, 0.015)]
+        [InlineData(4999, 0.015)]
+        [InlineData(5000, 0.02)]
+        [InlineData(9999, 0.02)]
+        [InlineData(10000, 0.025)]
+        [InlineData(49999, 0.025)]
+        [InlineData(50000, 0.03)]
+        [InlineData(500000, 0.03)]
+        public void CalculateInterestRateTestCases(decimal amount, double rate)
+        {
+            var account = new Account(amount);
+            Assert.Equal(rate, account.CalculateInterestRate());
+        }
+    }
+}
